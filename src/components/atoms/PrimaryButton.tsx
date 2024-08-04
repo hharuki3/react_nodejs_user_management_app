5458a23fc01fca7bce9485d000017f2b2b0733e6
hharuki3 <pingyanchunshu@gmail.com>
2024-08-04 23:47:08 +0900

    ログイン周りのstate管理・ボタン押下時イベント発火処理

--- src/components/atoms/PrimaryButton.tsx
+++ src/components/atoms/PrimaryButton.tsx
@@ -3,9 +3,10 @@ import { FC, memo, ReactNode } from "react";
 
 type Props = {
   children: ReactNode;
+  onClick: () => void;
 };
 
 export const PrimaryButton: FC<Props> = memo((props) => {
-  const { children } = props;
-  return <Button bg="teal.400" color="white" _hover={{opacity: 0.8}}>{children}</Button>;
+  const { children, onClick } = props;
+  return <Button onClick={onClick} bg="teal.400" color="white" _hover={{opacity: 0.8}}>{children}</Button>;
 });
--- src/components/organisms/LoginCard.tsx
+++ src/components/organisms/LoginCard.tsx
@@ -1,8 +1,14 @@
-import { FC, memo } from "react";
+import { FC, memo, useState } from "react";
 import { PrimaryButton } from "../atoms/PrimaryButton";
 import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 
 export const LoginCard: FC = memo(() => {
+  const [text, setText] = useState("");
+  const onClickLogin = () =>
+    // ここにaxiosを使ったログイン処理を実装
+    alert(`ログインしました：${text}`);
+  const onChangeText = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setText(e.target.value);
   return (
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
@@ -11,8 +17,12 @@ export const LoginCard: FC = memo(() => {
         </Heading>
         <Divider my={4} />
         <Stack spacing={4} py={4} px={10}>
-          <Input placeholder="ユーザーID" />
-          <PrimaryButton>ログイン</PrimaryButton>
+          <Input
+            placeholder="ユーザーID"
+            onChange={onChangeText}
+            value={text}
+          />
+          <PrimaryButton onClick={onClickLogin}>ログイン</PrimaryButton>
         </Stack>
       </Box>
     </Flex>
